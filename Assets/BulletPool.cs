be8cd94b082081f69932cdf5a655005d109aa536
HannesVernooij <hannesvernooy@hotmail.com>
2015-09-14 08:46:31 +0200

    SceneChanges & bullet pool

---
+++ Assets/BulletPool.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletPool : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _prefab;
+    private BulletScript[] _bulletPool = new BulletScript[100];
+
+    void Start()
+    {
+        GameObject temp;
+
+        for(int i = 0; i < 100; i ++)
+        {
+            temp = GameObject.Instantiate(_prefab);
+            _bulletPool[i] = temp.GetComponent<BulletScript>();
+            _bulletPool[i].transform.parent = gameObject.transform;
+            _bulletPool[i].SetActive(false);
+        }
+    }
+    
+    public void UseBullet(Vector3 startPosition, Quaternion rotation, GameObject fromObject)
+    {
+        for(int i = 0; i < _bulletPool.Length; i++)
+        {
+            if(_bulletPool[i].IsActive() == false)
+            {
+                _bulletPool[i].transform.position = startPosition;
+                _bulletPool[i].transform.rotation = fromObject.transform.rotation;
+                _bulletPool[i].SetActive(true);
+                break;
+            }
+        }
+    }
+}
---
+++ Assets/BulletScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour
+{
+    void Update()
+    {
+        transform.position += transform.forward * Time.deltaTime * 50;
+    }
+    public bool IsActive()
+    {
+        return gameObject.activeSelf;
+    }
+
+    public void SetActive(bool state)
+    {
+        gameObject.SetActive(state);
+    }
+
+    void OnCollisionEnter()
+    {
+        gameObject.transform.parent = gameObject.transform;
+        SetActive(false);
+    }
+}
