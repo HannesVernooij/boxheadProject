8cb74ff47262bebdfc46da378f4abb87e8aad600
sakirma <huseyin.32@live.nl>
2015-10-23 12:48:46 +0200

    EnemySpawn is af met een PoolingSysteem
    Wat er nog gedaan moet worden is dat de zombies met een navmesh achter
    de player aan gaan en nog de andere spawn plekken nog wat prefabs
    toevoegen zodat er over bij elke player een zombie spawned

---
+++ Assets/Scripts/EnemySpawnChildScript.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawnChildScript : MonoBehaviour
+{
+    [SerializeField]
+    private EnemySpawnScript _EnemySpawnScript;
+
+    public EnemySpawnScript GetEnemySpawnScript { get { return _EnemySpawnScript; } }
+    public State State { get { return m_State; } set { m_State = value; } }
+    public GameObject SetPlayer { set {m_Player = value;} }
+    public bool SetCanSpawn { set { m_CanSpawn = value; } }
+    
+    private State m_State;
+    private GameObject m_Player;
+    private bool m_CanSpawn;
+    private void Start()
+    {
+        _EnemySpawnScript = transform.parent.transform.GetComponent<EnemySpawnScript>();
+        m_State = new SpawnAllowed(this, transform);
+    }
+    private void Update()
+    {
+    }
+    private void OnTriggerStay(Collider coll)
+    {
+        if (m_CanSpawn == true && coll.gameObject == m_Player && _EnemySpawnScript.GetZombieSpawnLimit >= 0)
+        {
+            m_State.OnTriggerStay();
+        }
+    }
+}
+public interface State
+{
+    void Update();
+    void OnTriggerStay();
+    void Start();
+}
+
+public class SpawnAllowed : State
+{
+    private EnemySpawnChildScript spawn;
+    private Transform transform;
+    private float timer = 0;
+    public SpawnAllowed(EnemySpawnChildScript enemySpawnChildScript,Transform parentTransform)
+    {
+        spawn = enemySpawnChildScript;
+        transform = parentTransform;
+    }
+    public void Start()
+    {
+
+    }
+    public void Update()
+    {
+
+    }
+
+    public void OnTriggerStay()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            SpawnZombie();
+            spawn.GetEnemySpawnScript.GetZombieSpawnLimit -= 1;
+            timer = 2;
+        }
+    }
+    private void SpawnZombie()
+    {
+        foreach(GameObject go in spawn.GetEnemySpawnScript.GetZombies)
+        {
+            if(go.activeSelf == false)
+            {
+                go.SetActive(true);
+                go.transform.position = transform.position;
+                return;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/EnemySpawnScript.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawnScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_Player;
+    [SerializeField]
+    private GameObject[] m_ZombieAssets,
+                         m_ZombieSpawnPoints;
+
+    public GameObject ZombiesParent { get { return m_ZombiesEmtyGameObjectParent; } set { m_ZombiesEmtyGameObjectParent = value; } }
+    public GameObject[] GetZombies { get { return zombies; } }                   
+    public int GetZombieSpawnLimit { get { return m_ZombieSpawnLimit; } set { m_ZombieSpawnLimit = value; } }
+
+    private EnemySpawnChildScript[] _EnemySpawnChildScript;
+    private GameObject m_ZombiesEmtyGameObjectParent;
+    private GameObject[] zombies;
+    private int m_ZombieSpawnLimit = 30;
+
+    private void Start()
+    {
+        m_ZombieSpawnPoints = ZombieSpawnPointsSet();
+        CreateZombiesPool();
+    }
+    private void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject == m_Player)
+        {
+            foreach (EnemySpawnChildScript enemySpawnChildScript in _EnemySpawnChildScript)
+            {
+                enemySpawnChildScript.SetCanSpawn = true;
+            }
+        }
+    }
+    private void OnTriggerExit(Collider coll)
+    {
+        if (coll.gameObject == m_Player)
+        {
+            foreach (EnemySpawnChildScript enemySpawnChildScript in _EnemySpawnChildScript)
+            {
+                enemySpawnChildScript.SetCanSpawn = false;
+            }
+        }
+    }
+    //
+    private GameObject GettingTheChildSpawnZombieArea { get { return m_Player.transform.FindChild("ColliderSpawnZombieArea").gameObject; } }
+    private GameObject[] ZombieSpawnPointsSet()
+    {
+        m_ZombieSpawnPoints = new GameObject[transform.childCount];
+        _EnemySpawnChildScript = new EnemySpawnChildScript[transform.childCount];
+        GameObject[] temp = new GameObject[m_ZombieSpawnPoints.Length];
+        for (int i = 0; i < temp.Length; i++)
+        {
+            temp[i] = transform.GetChild(i).gameObject;
+            _EnemySpawnChildScript[i] = temp[i].AddComponent<EnemySpawnChildScript>();
+            _EnemySpawnChildScript[i].SetPlayer = GettingTheChildSpawnZombieArea;
+        }
+        return temp;
+    }
+    //
+    private void CreateZombiesPool()
+    {
+        GameObject zombiesParent = new GameObject("ZombiesParent");
+        zombiesParent.transform.SetParent(transform);
+        zombies = new GameObject[m_ZombieSpawnLimit];
+        for (int i = 0; i < m_ZombieSpawnLimit; i++)
+        {
+            zombies[i] = new GameObject("zombie" + i);
+            zombies[i].transform.SetParent(zombiesParent.transform);
+            AddingComponentsToZombies(zombies[i]);
+            zombies[i].SetActive(false);
+        }
+    }
+    private void AddingComponentsToZombies(GameObject zombie)
+    {
+        int r = Random.Range(0, 3);
+        GameObject randomZombieAsset = m_ZombieAssets[r];
+
+        MeshFilter MF = zombie.AddComponent<MeshFilter>();
+        MeshRenderer MR = zombie.AddComponent<MeshRenderer>();
+
+        MF.mesh = randomZombieAsset.GetComponent<MeshFilter>().sharedMesh;
+        MR.material = randomZombieAsset.GetComponent<MeshRenderer>().sharedMaterial;
+
+        zombie.transform.rotation = randomZombieAsset.transform.rotation;
+    }
+    //
+}
