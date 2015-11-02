536227cb35746c33d4d464dd989140f10808ea5b
sakirma <huseyin.32@live.nl>
2015-11-02 15:30:18 +0100

    soundSystem script done

--- Assets/Scripts/SoundSystem.cs
+++ Assets/Scripts/SoundSystem.cs
@@ -21,24 +21,77 @@ public class SoundSystem : MonoBehaviour
             m_AudioSource[i] = m_Locations[i].AddComponent<AudioSource>();
         }
     }
-
-    public void Shoot(int player, int gun)
+    public void Shoot(int player, string weaponTag)
     {
-        switch (gun)
+        switch (weaponTag)
         {
-            case 0://Pistol
+            case "Pistol":
                 ShootPistol(player);
                 break;
-            case 1://Shotgun
+            case "Shotgun":
                 ShootShotgun(player);
                 break;
-            case 2://AK
+            case "smg":
                 ShootAK(player);
                 break;
-            case 3://Sniper
+            case "Sniper":
                 ShootSniper(player);
                 break;
         }
+    }
+
+    public void Reload(int player,string weaponTag)
+    {
+        switch (weaponTag)
+        {
+            case "Pistol":
+                StartCoroutine(ReloadPistol(player));
+                break;
+            case "Shotgun":
+                ReloadShotgun(player);
+                break;
+            case "smg":
+                StartCoroutine(ReloadAK(player));
+                break;
+            case "Sniper":
+                StartCoroutine(ReloadSniper(player));
+                break;
+        }
+    }
+
+    private void ShootShotgun(int player)
+    {
+        m_AudioSource[player].clip = m_Shotgun[0];
+        m_AudioSource[player].Play();
+    }
+    private void ReloadShotgun(int player)
+    {
+        m_AudioSource[player].clip = m_Shotgun[1];
+        m_AudioSource[player].Play();
+    }
+    public void ShotgunPump(int player)
+    {
+        m_AudioSource[player].clip = m_Shotgun[2];
+    }
+    private void ShootAK(int player)
+    {
+        m_AudioSource[player].clip = m_AK[0];
+        m_AudioSource[player].Play();
+    }
+
+    private IEnumerator ReloadAK(int player)
+    {
+        m_AudioSource[player].clip = m_AK[1];
+        m_AudioSource[player].Play();
+        yield return new WaitForSeconds(m_AK[1].length + 0.2f);
+        m_AudioSource[player].clip = m_AK[3];
+        m_AudioSource[player].Play();
+        yield return new WaitForSeconds(m_AK[3].length + 0.3f);
+        m_AudioSource[player].clip = m_AK[2];
+        m_AudioSource[player].Play();
+        yield return new WaitForSeconds(m_AK[2].length + 0.25f);
+        m_AudioSource[player].clip = m_AK[1];
+        m_AudioSource[player].Play();
     }
 
     private void ShootPistol(int player)
@@ -47,23 +100,40 @@ public class SoundSystem : MonoBehaviour
         m_AudioSource[player].Play();
     }
 
-    private void ShootShotgun(int player)
+    private IEnumerator ReloadPistol(int player)
     {
-
-        m_AudioSource[player].clip = m_Shotgun[0];
+        m_AudioSource[player].clip = m_Pistol[3];
         m_AudioSource[player].Play();
-
-    }
-
-    private void ShootAK(int player)
-    {
-        m_AudioSource[player].clip = m_AK[0];
+        yield return new WaitForSeconds(m_Pistol[3].length + 0.1f);
+        m_AudioSource[player].clip = m_Pistol[2];
+        m_AudioSource[player].Play();
+        yield return new WaitForSeconds(m_Pistol[2].length + 0.1f);
+        m_AudioSource[player].clip = m_Pistol[1];
+        m_AudioSource[player].Play();
+        yield return new WaitForSeconds(m_Pistol[1].length + 0.1f);
+        m_AudioSource[player].clip = m_Pistol[4];
         m_AudioSource[player].Play();
     }
-
     private void ShootSniper(int player)
     {
         m_AudioSource[player].clip = m_Sniper[0];
+        m_AudioSource[player].Play();
+    }
+    private IEnumerator ReloadSniper(int player)
+    {
+        m_AudioSource[player].clip = m_Sniper[1];
+        m_AudioSource[player].Play();
+        yield return new WaitForSeconds(m_Sniper[1].length + 0.1f);
+        m_AudioSource[player].clip = m_Sniper[5];
+        m_AudioSource[player].Play();
+        yield return new WaitForSeconds(m_Sniper[5].length + 0.3f);
+        m_AudioSource[player].clip = m_Sniper[4];
+        m_AudioSource[player].Play();
+        yield return new WaitForSeconds(m_Sniper[4].length - 0.1f); //Cliphit voert uit terwijl clipin uitvoert.
+        m_AudioSource[player].clip = m_Sniper[3];
+        m_AudioSource[player].Play();
+        yield return new WaitForSeconds(m_Sniper[3].length + 0.1f);
+        m_AudioSource[player].clip = m_Sniper[2];
         m_AudioSource[player].Play();
     }
 }
